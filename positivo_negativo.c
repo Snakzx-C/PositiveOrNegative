4634bc580b92d32a3e68fd3f964a88bfbc764d5f
SN4KX00! <82485594+Snakzx-C@users.noreply.github.com>
2021-05-12 05:46:29 -0300

    Update positivo_negativo.c

--- positivo_negativo.c
+++ positivo_negativo.c
@@ -6,25 +6,30 @@
 
 int main(){
   system("clear"); //limpa o console
+  printf("Iniciando programa...\n\n");
+  sleep(1);
 
   int num; //declaro a variavel (num)
   printf("Digite um número: "); //imprimo o texto escolhido na tela (sujeito a modificações ou inserções)
   scanf("%d", &num); // inserção de texto
 
-  if (num == 0){
-      printf("o número é neutro\n");
+  if (num == 0){ //verifica se o numero é igual a zero
+      printf("o número é neutro\n\n");
   }
-  else
+  else //se o numero nao for igual a zero ele parte para as linhas de comando abaixo
   {
-    if (num > 0)
+    if (num > 0) //verfica se o numero é maior que zero 
     {
-      printf("o número é positivo\n");
+      printf("o número é positivo\n\n");
     }
-    else
+    else //verifica se o numero é negativo
     {
-      printf("o número é negativo\n");
+      printf("o número é negativo\n\n");
     }
     
   }
+
+  printf("fim do programa...\n\n");
+  sleep(1);
 
 }
